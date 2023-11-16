07c280663256494b60aabede1cd37bde18222a00
JainenBAlbornoz <jainenbanqueroalbornoz@gmail.com>
2023-11-16 20:25:48 -0300

    falta-js-pages

---
+++ main.js
@@ -0,0 +1,21 @@
+let nombre_usuario = prompt("¡Hola! ¿Cómo te llamas?");
+
+console.log(nombre_usuario);
+
+alert("¡Hola" + nombre_usuario + "bienvenido a Cultmovies!");
+
+function mensaje(){
+    alert("Iniciar sesión/configuración")
+};
+document.getElementById("boton").addEventListener("click", mensaje)
+
+document.getElementById("titulopagina");
+
+let paginatitulo = document.getElementById("titulopagina");
+
+console.log(paginatitulo);
+
+let contenidotitulo = paginatitulo.innerText ;
+console.log(contenidotitulo);
+
+paginatitulo.innerText ="Cine de culto";
